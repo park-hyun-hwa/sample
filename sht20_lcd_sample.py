19a38f92c5bf168f8837d2470b2acc31ef245ee6
park-hyun-hwa <ydj00123@nate.com>
2015-07-08 16:25:07 +0900

    Update sht20_lcd_sample.py

--- sht20_lcd_sample.py
+++ sht20_lcd_sample.py
@@ -47,7 +47,7 @@ logger = logging.getLogger(sensorname)
 
 ##### open RASPI serial device, 38400#########
 try: 
-    serial_in_device = serial.Serial('/dev/ttyAMA0',38400)
+    serial_in_device = serial.Serial('/dev/ttyAMA0',38400) ##38400bps 
 except serial.SerialException, e:
     logger.error("Serial port open error") 
     ledall_off()
