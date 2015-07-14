d5fe3758d54e5143c396dc429d154c2dce27d7f8
park-hyun-hwa <ydj00123@nate.com>
2015-07-14 15:29:39 +0900

    Create sound_test.py

---
+++ sound_test.py
@@ -0,0 +1,11 @@
+import pygame
+
+if __name__ == '__main__':
+  try:
+    pygame.mixer.init()
+    pygame.mixer.music.load("sound_test.mp3")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+      continue
+  except KeyboardInterrupt:
+    pass
