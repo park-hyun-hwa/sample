4ee79deca9e0530340e9aaca3531441ee45cbd22
park-hyun-hwa <ydj00123@nate.com>
2015-07-14 15:50:45 +0900

    Update sound_test.py

--- sound_test.py
+++ sound_test.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     pygame.mixer.init()
     pygame.mixer.music.load("sound_test.mp3")
     pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-      continue
+    #while pygame.mixer.music.get_busy() == True:
+      #continue
   except KeyboardInterrupt:
     pass
