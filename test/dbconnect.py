40ea2dee27fb3edeeb1c93e70e619b07a713f802
park-hyun-hwa <ydj00123@nate.com>
2015-07-22 13:06:30 +0900

    Create dbconnect.py

---
+++ test/dbconnect.py
@@ -0,0 +1,16 @@
+import MYSQLdb as mdb
+import sys
+
+try:
+  con = mdb.connect('localhost','root','tinyos','keti')
+  
+  cur = con.cursor()
+  sql = "SELECT * FROM onenode" 
+  cur.execute(sql)
+  for i in range(cur.rowcount):
+    row = cur.fetchone()
+    print row[0]
+
+except mdb.Error,e:
+  print "Error"
+  sys.exit(1)
